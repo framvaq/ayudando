3124a506df393d7726c51965b27f52edb3b14b8e
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-03 19:57:22 +0200

    a bit of work left

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CreateComponent } from './create.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  declarations: [CreateComponent],
+  imports: [CommonModule, FormsModule]
 })
 export class CreateModule {}
