997d99bfeb7f852af176b5cf8866d18ab4f42332
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-04 12:00:31 +0200

    fixed login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('./login/login.module') },
+  { path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'inicio', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   {
     path: 'anuncios',
