cadd03b64e0b19ba8f8e1624f951cad79d9496b8
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-05 10:50:04 +0200

    added annoucemenT folder

--- src/app/announcements/announcements.module.ts
+++ src/app/announcements/announcements.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AnnouncementsRoutingModule } from './announcements-routing.module';
 import { AnnouncementsComponent } from './announcements.component';
+import { AnnouncementComponent } from './announcement/announcement.component';
 
 @NgModule({
-  declarations: [AnnouncementsComponent],
+  declarations: [AnnouncementsComponent, AnnouncementComponent],
   imports: [CommonModule, AnnouncementsRoutingModule]
 })
 export class AnnouncementsModule {}
