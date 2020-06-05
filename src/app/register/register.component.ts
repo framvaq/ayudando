26f9a789f0ec5bc52fe6e5f06554b06eae2214d1
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-05 17:57:47 +0200

    todo: add logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,6 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   constructor() {}
-
+  //Add radio buttons logic (if "no" is selected, it acts as a normal radio (one choice), else, multiple choice)
   ngOnInit(): void {}
 }
