66714151975661f8af5d4b03680d5b073066cd50
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-05 09:34:20 +0200

    very simple grid (no helpers)

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  exports: [MatToolbarModule]
+  exports: [MatInputModule]
 })
 export class AppMaterialModule {}
--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create.component';
 
 @NgModule({
   declarations: [CreateComponent],
-  imports: [CommonModule, CreateRoutingModule, FormsModule]
+  imports: [CommonModule, CreateRoutingModule, FormsModule, ReactiveFormsModule]
 })
 export class CreateModule {}
