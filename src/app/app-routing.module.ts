e0481ff575338c4eea5d1c03600c8ae008cad467
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-05 13:05:09 +0200

    minor route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'inicio', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   {
@@ -15,7 +15,7 @@ const routes: Routes = [
   },
   { path: 'panel-usuario', loadChildren: () => import('./userPanel/userPanel.module').then(m => m.UserPanelModule) },
   { path: 'nuevo-anuncio', loadChildren: () => import('./create/create.module').then(m => m.CreateModule) },
-  { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
+  { path: 'registro', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
   { path: 'buscar', loadChildren: () => import('./search/search.module').then(m => m.SearchModule) },
   { path: '**', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) }
 ];
