4dfe3cda0ce411b126fdf16104ce5741c88cfe8a
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-07 17:31:13 +0200

    created basic small announcement template

---
+++ src/app/templates/small-announcement/small-announcement-template.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-announcement-template',
+  templateUrl: 'small-announcement-template.component.html',
+  styleUrls: ['small-announcement-template.component.scss']
+})
+export class SmallAnnouncementTemplateComponent {}
