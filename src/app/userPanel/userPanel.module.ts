0795d59aacd6bcbdbd4c32204b390f1a709c9ff9
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-09 14:35:22 +0200

    wip

--- src/app/userPanel/userPanel.module.ts
+++ src/app/userPanel/userPanel.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UserPanelRoutingModule } from './userPanel-routing.module';
 import { UserPanelComponent } from './userPanel.component';
 import { LayoutsModule } from '../layouts/layouts.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UserPanelComponent],
-  imports: [CommonModule, UserPanelRoutingModule, LayoutsModule]
+  imports: [CommonModule, UserPanelRoutingModule, LayoutsModule, FormsModule]
 })
 export class UserPanelModule {}
