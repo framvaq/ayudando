dcd5cc7654870ac3d5e97a236b2d328f92f5485d
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-09 09:48:51 +0200

    added getNumberOfAnnouncements()

--- src/app/helpers/services/announcements.service.ts
+++ src/app/helpers/services/announcements.service.ts
@@ -10,10 +10,26 @@ import { ANNOUNCEMENTS } from '../../mock-announcements';
 })
 export class AnnouncementsService {
   constructor() {}
+  /**
+   * Returns all the announcements
+   */
   getAnnouncements() {
     return ANNOUNCEMENTS;
   }
 
+  /**
+   * Returns the specified number of announcements
+   * @param num Number of announcements to return
+   */
+  getNumberOfAnnouncements(num) {
+    console.log(ANNOUNCEMENTS.filter(announce => announce.id <= num));
+    return ANNOUNCEMENTS.filter(announce => announce.id <= num);
+  }
+
+  /**
+   * Returns the announcements with the specified ID
+   * @param id The id from the announcement I want to retrieve
+   */
   getAnnouncementsById(id) {
     return ANNOUNCEMENTS[--id];
   }
