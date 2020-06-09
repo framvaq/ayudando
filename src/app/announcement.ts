61385dae3a1d1cc8863d9f037a3c0a1f31ae7a27
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-09 09:53:15 +0200

    added example in comment

--- src/app/announcement.ts
+++ src/app/announcement.ts
@@ -6,3 +6,28 @@ export interface Announcement {
   place?: string;
   type: string;
 }
+/*
+{
+  id: 1,
+  title: 'Example title01',
+  description: 'Exampledescription01',
+  date: new Date(2021, 0, 13),
+  place: 'Example place',
+  type: 'Recado'
+},
+{
+  id: 2,
+  title: 'Example title02',
+  description: 'Exampledescription02',
+  date: new Date(2021, 11, 3),
+  type: 'Recado'
+},
+{
+  id: 3,
+  title: 'Example title03',
+  description: 'Exampledescription03',
+  date: new Date(2021, 11, 14),
+  place: 'Example place',
+  type: 'Recado'
+}
+*/
