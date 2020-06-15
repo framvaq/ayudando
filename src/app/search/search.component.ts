f4edfe6c24923b578b2ff703f89626d20420aeea
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-15 21:54:02 +0200

    refactor

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { AnnouncementsService } from '../services/announcements.service';
 
 @Component({
@@ -7,15 +9,18 @@ import { AnnouncementsService } from '../services/announcements.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  filters = '';
-  results;
-  numberOfAnnouncements;
   announcements;
-  announcementsById;
-  announcementsLimitedByNumber;
-  announcementsFiltered;
 
-  constructor(private announcementsService: AnnouncementsService) {}
+  filters = this.formBuilder.group({
+    type: [null],
+    date: [null]
+    // place: [null]
+  });
+
+  numberOfAnnouncements;
+  announcementsLimitedByNumber;
+
+  constructor(private formBuilder: FormBuilder, private announcementsService: AnnouncementsService) {}
 
   ngOnInit(): void {
     console.log('getannouncements');
@@ -27,4 +32,25 @@ export class SearchComponent implements OnInit {
   getAnnouncements() {
     return this.announcementsService.getAnnouncements().subscribe(result => (this.announcements = result));
   }
+
+  /*********************************************************************
+   *************************** FILTERS LOGIC ***************************
+   *********************************************************************/
+
+  submit() {
+    console.log('Valor de filters:', this.filters.value);
+    console.log('Valor de type:', this.filters.value.type);
+  }
+
+  getAnnouncementsLimitedByNumber() {
+    return this.announcementsService
+      .getNumberOfAnnouncements(this.numberOfAnnouncements)
+      .subscribe(result => (this.announcementsLimitedByNumber = result));
+  }
+  /*
+
+
+
+
+  */
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../app-material.module';
 import { SearchRoutingModule } from './search-routing.module';
@@ -9,6 +10,15 @@ import { SearchComponent } from './search.component';
 
 @NgModule({
   declarations: [SearchComponent],
-  imports: [CommonModule, SearchRoutingModule, LayoutsModule, AppMaterialModule, FiltersModule]
+  imports: [
+    CommonModule,
+    SearchRoutingModule,
+    LayoutsModule,
+    AppMaterialModule,
+    /*
+    FiltersModule, 
+    */
+    ReactiveFormsModule
+  ]
 })
 export class SearchModule {}
