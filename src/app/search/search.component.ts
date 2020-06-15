f58e3565278fb9f5a9a0fc9d1e108ac070aa4ae2
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-15 22:29:31 +0200

    todo: more info on checkboxes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -12,8 +12,8 @@ export class SearchComponent implements OnInit {
   announcements;
 
   filters = this.formBuilder.group({
-    type: [null],
-    date: [null]
+    type: [null], // TODOchange according to userType (if both, value = null)
+    date: [null] // WIP
     // place: [null]
   });
 
@@ -40,6 +40,7 @@ export class SearchComponent implements OnInit {
   submit() {
     console.log('Valor de filters:', this.filters.value);
     console.log('Valor de type:', this.filters.value.type);
+    console.log('Valor de date:', this.filters.value.date);
   }
 
   getAnnouncementsLimitedByNumber() {
