4c5919cfc6ab20a170662fe7f4627c5c17be50a5
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-16 09:55:05 +0200

    deleted: filtersModule -> Integrated in searchModule

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -5,20 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppMaterialModule } from '../app-material.module';
 import { SearchRoutingModule } from './search-routing.module';
 import { LayoutsModule } from '../layouts/layouts.module';
-import { FiltersModule } from './filters/filters.module';
 import { SearchComponent } from './search.component';
 
 @NgModule({
   declarations: [SearchComponent],
-  imports: [
-    CommonModule,
-    SearchRoutingModule,
-    LayoutsModule,
-    AppMaterialModule,
-    /*
-    FiltersModule, 
-    */
-    ReactiveFormsModule
-  ]
+  imports: [CommonModule, SearchRoutingModule, LayoutsModule, AppMaterialModule, ReactiveFormsModule]
 })
 export class SearchModule {}
