85138ef24d7e393501daf1ead7590c335659c490
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-18 12:48:37 +0200

    todo: manage errors

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { SubmitService } from '../services/submit.service';
+import { Communities } from '../assets/places';
+import { PLACES } from '../assets/places-values';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +11,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
-  // Add radio buttons logic (if "no" is selected, it acts as a normal radio (one choice), else, multiple choice)
+  data = this.formBuilder.group({
+    email: [null, Validators.required],
+    pass: [null, Validators.required],
+    passver: [null, Validators.required],
+    place: [null, Validators.required],
+    user: [null, Validators.required],
+    type: [null, Validators.required],
+    contact: this.formBuilder.group(
+      {
+        no: [false],
+        whencontact: [false],
+        whenannouncement: [false]
+      },
+      [Validators.required]
+    )
+  });
+
+  requiredErrorMessage = 'Este campo es obligatorio';
+
+  communities: Communities[] = PLACES;
+
+  constructor(private formBuilder: FormBuilder, private submitService: SubmitService) {}
+
   ngOnInit(): void {}
+
+  getRequiredErrorMessage() {
+    if (this.data.value.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.data.value.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  submit() {
+    // console.log(this.data.value.email);
+
+    if (this.validate()) {
+      console.log('data.value = ', this.data.value);
+    }
+  }
+
+  validate() {
+    return (
+      this.validateEmail(this.data.value.email) &&
+      this.validatePass(this.data.value.pass) &&
+      this.validatePassver(this.data.value.pass, this.data.value.passver) &&
+      this.validatePlace(this.data.value.place) &&
+      this.validateUser(this.data.value.user) &&
+      this.validateType(this.data.value.type) &&
+      this.validateContact(this.data.value.contact)
+    );
+  }
+
+  validateEmail(mail) {
+    // Regular expression inspired by https://ihateregex.io/
+    // Returns true for anything but @, space, tabs, new lines and accentuated letters in those groups: []@[].[]
+    return /[^@ \t\r\náéíóúÁÉÍÓÚ]+@[^@ \t\r\náéíóúÁÉÍÓÚ]+\.[^@ \t\r\náéíóúÁÉÍÓÚ]+/.test(mail);
+  }
+
+  validatePass(pass) {
+    // Regular expression inspired by https://ihateregex.io/
+    // At least: 8 chars, one uppercase, one lowercase, one number and one special charachter(#?!@$ %^&*-)
+    return /^(?=.*?[A-ZÇÑ])(?=.*?[a-zçñ])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/.test(pass);
+  }
+
+  validatePassver(pass, passver) {
+    return pass === passver;
+  }
+
+  validatePlace(place) {
+    return place !== null && place !== undefined;
+  }
+
+  validateUser(user) {
+    // Accepts lower and upper case, _ and -, with a minimum of 3 chars and max of 20
+    return /^[a-zA-ZñÑçÇ0-9_-]{3,20}$/.test(user);
+  }
+
+  validateType(type) {
+    return type === 'ask' || type === 'give' || type === 'both';
+  }
+
+  validateContact(contact) {
+    // console.log(contact.no);
+    return contact.no || contact.whencontact || contact.whenannouncement;
+  }
 }
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { AppMaterialModule } from '../app-material.module';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RegisterRoutingModule]
+  imports: [CommonModule, RegisterRoutingModule, ReactiveFormsModule, AppMaterialModule]
 })
 export class RegisterModule {}
